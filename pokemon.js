163133e92284fa50a1e910a08a748c4ea917d0a5
Lera Neroznak <76168462+Lera2004@users.noreply.github.com>
2024-01-26 10:08:50 +0200

    Add files via upload

---
+++ pokemon.js
@@ -0,0 +1,53 @@
+// pokemon.js
+export class Pokemon {
+  constructor(name, health, $hpElement, $progressbar) {
+    this.name = name;
+    this.defaultHealth = health;
+    this.damageHP = health;
+    this.$hpElement = $hpElement;
+    this.$progressbar = $progressbar;
+  }
+
+  init() {
+    this.renderHP();
+  }
+
+renderHP() {
+    this.$hpElement.innerText = this.damageHP + ' / ' + this.defaultHealth;
+    this.$progressbar.style.width = (this.damageHP / this.defaultHealth) * 100 + '%';
+
+    if (this.damageHP < 60 && this.damageHP > 20) {
+        this.$progressbar.classList.add('low');
+    } else {
+        this.$progressbar.classList.remove('low');
+    }
+
+
+    if (this.damageHP < 20) {
+        this.$progressbar.classList.add('critical');
+    } else {
+        this.$progressbar.classList.remove('critical');
+    }
+}
+
+
+  changeHP(count) {
+    this.damageHP -= count;
+
+    if (this.damageHP <= 0) {
+      this.damageHP = 0;
+    }
+
+    this.renderHP();
+    return this.damageHP;
+  }
+
+  reset() {
+    this.damageHP = this.defaultHealth;
+    this.renderHP();
+  }
+
+  isDefeated() {
+    return this.damageHP === 0;
+  }
+}
